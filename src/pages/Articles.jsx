4d60cc536db7b0c48d1c89043439e076fec10dcd
Wangwei <2219444825@qq.com>
2022-05-17 21:28:16 +0800

    文章列表页排序功能

--- src/pages/Articles.jsx
+++ src/pages/Articles.jsx
@@ -4,6 +4,18 @@ import { useState, useEffect } from 'react'
 import { Spin, Pagination } from 'antd'
 import IconFont from '../components/IconFont'
 
+function comparsionFn(key, rule) {
+  return (a, b) => {
+    if (rule === -1) {
+      ;[b, a] = [a, b]
+    }
+    if (key === 'date') {
+      return new Date(a.create_at).getTime() - new Date(b.create_at).getTime()
+    }
+    return a[key] - b[key]
+  }
+}
+
 function Articles() {
   const [articles, setArticles] = useState({
     list: [],
@@ -15,12 +27,17 @@ function Articles() {
   })
   // 控制加载状态
   const [loading, setLoading] = useState(false)
+  // 排序
+  const [sortKey, setSortKey] = useState('date') // 排序关键字 date or read
+  const [sortRule, setSortRule] = useState(1) // 排序规则 1->升序 -1->降序
+
+  // 两个useEffect执行时机？？？
   useEffect(() => {
     async function loadData() {
       setLoading(true)
       // 文章数据
       const result = await fetchArticleList(pageParams.page, pageParams.limit)
-      console.log(result.count)
+      result.data = result.data.sort(comparsionFn(sortKey, sortRule))
       setArticles({
         list: result.data,
         count: result.count,
@@ -29,6 +46,15 @@ function Articles() {
     }
     loadData()
   }, [pageParams])
+
+  // 排序关键字或排序规则 产生的副作用
+  useEffect(() => {
+    const sortArticlesList = articles.list.sort(comparsionFn(sortKey, sortRule))
+    setArticles({
+      ...articles,
+      list: sortArticlesList,
+    })
+  }, [sortKey, sortRule])
 
   // 页码发生改变
   function pageChange(page) {
@@ -40,7 +66,34 @@ function Articles() {
 
   return (
     <div className="md:px-20 px-4 py-2">
-      <div className="my-10"></div>
+      <div className="my-10 grid grid-cols-3 w-full md:w-3/5">
+        <button
+          className={`text-gray-400 flex items-center ${
+            sortKey === 'date' ? 'text-gray-900' : ''
+          }`}
+          onClick={() => setSortKey('date')}>
+          <IconFont type="icon-time" className="mr-2" />
+          <span className="text-lg">按发布日期排序</span>
+        </button>
+        <button
+          className={`text-gray-400 flex items-center ${
+            sortKey === 'read' ? 'text-gray-900' : ''
+          }`}
+          onClick={() => setSortKey('read')}>
+          <IconFont type="icon-paixu" className="mr-2" />
+          <span className="text-lg">按阅读量排序</span>
+        </button>
+        <button
+          className="text-gray-900 flex items-center"
+          onClick={() => setSortRule(-sortRule)}>
+          {sortRule > 0 ? (
+            <IconFont type="icon-shengxu" className="mr-2" />
+          ) : (
+            <IconFont type="icon-jiangxu" className="mr-2" />
+          )}
+          <span className="text-lg">{sortRule > 0 ? '升序' : '降序'}</span>
+        </button>
+      </div>
       <Spin spinning={loading} size="large">
         <div className="grid grid-cols-1 md:grid-cols-2 gap-20 min-h-[400px]">
           {articles.list.map((item) => (
